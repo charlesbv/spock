20405e50ef675b83f038051c36856bba370d2dc1
Charles Bussy-Virat <bussyvirat@gmail.com>
2019-02-20 10:19:19 -0500

    ign

---
+++ srcPython/beacon_find_best_prn.py
@@ -0,0 +1,11 @@
+# For a given overpass of CYGNSS over the beacon station in Holloman, NM, this script selects the opitmum specular point, based on certain criteria (see in code what those criterias are)
+# Inputs:
+# - prefix_pass = prefix of pass to look at. Format: 'pass_N' where N is the overpass to manually select
+# - csv_dir = directory of the csv files output by tds-bop
+
+
+# PARAMETERS TO SET UP BEFORE RUNNING THIS SCRIPT
+prefix_pass = 'pass_9'
+csv_dir
+
+# end of PARAMETERS TO SET UP BEFORE RUNNING THIS SCRIPT
