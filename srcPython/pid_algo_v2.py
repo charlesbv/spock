9d64a43c6e34f240de6975aaa4f06fdc4f157a61
cbv <cbv@srbwks2014-0008.engin.umich.edu>
2019-03-29 16:37:24 -0400

    ign

--- srcPython/pid_algo_v2.py
+++ srcPython/pid_algo_v2.py
@@ -20,7 +20,7 @@ rho_more = 'mid' # equator, pole, mid -> where to add more rho (pole means the i
 isbig = 0 # if runnign script from Big
 ispleiades = 0 # if runnign script from Pleaides
 dir_simu = '/Users/cbv/work/spockOut/density' # directory where SpOCK simu are run (input and output files)
-no_prop = 1 # set this variable to 1 to prevent creating SpOCK main input files and propagating them
+no_prop = 0 # set this variable to 1 to prevent creating SpOCK main input files and propagating them
 interval = 18.0 #18.0 # interval of time to compare the two trajectories (data and SpOCK). In hours
 step_move_save = 3.0
 step_drho = 0.1 # the rho control will vary by this amount to find the optimum rho over an interval
@@ -29,7 +29,7 @@ kdlist = [1.] # list of derivative gains for PID
 kilist = [0.000] # list of integral gains for PID
 plot_or_not = 1
 inter_start_algo = 1.0
-prefix_name = 'gravMap_sp13'
+prefix_name = 'fastearth_sp13'
 #'grav80'#'rho0_grav50_solarzenith'#'dt0_1s_solarzenith'
 #'grav50_solarzenith'#'solarzenith'#localtime70percent'
 # end of PARAMETERS TO SET UP BEFORE RUNNIG THIS SCRIPT
@@ -365,7 +365,7 @@ for iinter in range(nb_interval):#!!!!! shoul be nb_interval):
             #if ((iinter > 0) | ((iinter== 0) & (irho >=2))):
                 if ispleiades != 1:
                     #os.system(path_mpirun + ' -np 1 spock_dev ' + main_input_filename)
-                    os.system(path_mpirun + ' -np 1 spock_grav_read_bin ' + main_input_filename)
+                    os.system(path_mpirun + ' -np 1 spock_grav_read_bin_earth_map ' + main_input_filename)
                 else:
                     os.system(path_mpirun + ' /home1/cbussy/spock ' + main_input_filename)
 
