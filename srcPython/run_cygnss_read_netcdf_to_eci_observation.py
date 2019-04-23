630bd04cf1f00cdf0ea00835cb296b9334a729dd
Charles Bussy-Virat <bussyvirat@gmail.com>
2019-04-23 10:10:59 -0400

    ign

--- srcPython/run_cygnss_read_netcdf_to_eci_observation.py
+++ srcPython/run_cygnss_read_netcdf_to_eci_observation.py
@@ -1,16 +1,16 @@
 import sys
 sys.path.append("/Users/cbv/work/spock/srcPython")
 from cygnss_read_netcdf_to_eci_observation import *
-filename_list = ['013/cyg03.ddmi.s20180113-000000-e20180113-235959.l1.power-brcs.a21.d21.nc',
-                 '014/cyg03.ddmi.s20180114-000000-e20180114-235959.l1.power-brcs.a21.d21.nc',
-                 '015/cyg03.ddmi.s20180115-000000-e20180115-235959.l1.power-brcs.a21.d21.nc',
-                 '016/cyg03.ddmi.s20180116-000000-e20180116-235959.l1.power-brcs.a21.d21.nc',
-                 '017/cyg03.ddmi.s20180117-000000-e20180117-235959.l1.power-brcs.a21.d21.nc',
-                 '018/cyg03.ddmi.s20180118-000000-e20180118-235959.l1.power-brcs.a21.d21.nc',
-                 '019/cyg03.ddmi.s20180119-000000-e20180119-235959.l1.power-brcs.a21.d21.nc',
-                 '020/cyg03.ddmi.s20180120-000000-e20180120-235959.l1.power-brcs.a21.d21.nc',
-                 '021/cyg03.ddmi.s20180121-000000-e20180121-235959.l1.power-brcs.a21.d21.nc',
-                 '022/cyg03.ddmi.s20180122-000000-e20180122-235959.l1.power-brcs.a21.d21.nc',
+filename_list = ['229/cyg01.ddmi.s20170817-000000-e20170817-235959.l1.power-brcs.a21.d21.nc',
+                 '230/cyg01.ddmi.s20170818-000000-e20170818-235959.l1.power-brcs.a21.d21.nc',
+                 '231/cyg01.ddmi.s20170819-000000-e20170819-235959.l1.power-brcs.a21.d21.nc',
+                 '232/cyg01.ddmi.s20170820-000000-e20170820-235959.l1.power-brcs.a21.d21.nc',
+                 '233/cyg01.ddmi.s20170821-000000-e20170821-235959.l1.power-brcs.a21.d21.nc',
+                 '234/cyg01.ddmi.s20170822-000000-e20170822-235959.l1.power-brcs.a21.d21.nc',
+                 '235/cyg01.ddmi.s20170823-000000-e20170823-235959.l1.power-brcs.a21.d21.nc',
+                 '236/cyg01.ddmi.s20170824-000000-e20170824-235959.l1.power-brcs.a21.d21.nc',
+                 '237/cyg01.ddmi.s20170825-000000-e20170825-235959.l1.power-brcs.a21.d21.nc',
+                 '238/cyg01.ddmi.s20170826-000000-e20170826-235959.l1.power-brcs.a21.d21.nc',
 ]
 
 cygnss_read_netcdf_to_eci_observation(filename_list)
