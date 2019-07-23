f98bf80314d0da6df3a7546ca1efb48aa2ee889f
cbv <cbv@srbwks2014-0008.engin.umich.edu>
2019-07-23 11:42:59 -0400

    ign

--- srcPython/cygnss_main_camp2ex.py
+++ srcPython/cygnss_main_camp2ex.py
@@ -17,19 +17,19 @@ import sys
 sys.path.append('/Users/cbv/work/spock/srcPython')
 from spock_main_input import *
 
-date_start = sys.argv[1]
-date_stop = sys.argv[2]
+date_start = sys.argv[1] #'2019-07-21'#sys.argv[1]
+date_stop = sys.argv[2] #'2019-07-22'#sys.argv[2]
 
 print date_start, date_stop
 # Download the CYGNSS TLEs for date_start
-#os.system("cygnss_tle_web.py " +   date_start)
+os.system("cygnss_tle_web.py " +   date_start)
 cygnss_tle_filename = 'cygnss_' + date_start + '.txt'
 
 # Run SpOCK between date_start and date_stop
 date_start_with_time = date_start + 'T00:00:00'
 date_stop_with_time = date_stop + 'T00:00:00'
 dt_simu = 1.
-order_gravity = 0 # !!!!!!! put 20
+order_gravity = 20 # !!!!!!! put 20
 forces = 'drag moon_gravity sun_gravity'
 main_input_filename = 'camp2exTest.txt'
 spock_main_input(
@@ -48,7 +48,7 @@ spock_main_input(
     # for FORCES section
     order_gravity,
     forces,
-    'static',
+    'static', # !!!!!!!!make sure the static values are consistent with the current trend of f107 and ap
     # for OUTPUT section
     '~/cygnss/camp2ex/out/out',
     dt_simu,
@@ -61,7 +61,7 @@ spock_main_input(
     # for DENSITY_MOD section
     1
 )
-#os.system("mpirun -np 4 spock " + main_input_filename)
+os.system("mpirun -np 4 spock_grav_read_bin_earth_map " + main_input_filename) # !!!!!!! make sure this is the latestest executable (spock on my laptop...)
 
 # Run cygnss_camp2ex.py to output the lon/lat/alt and solar zenith angle
 os.system('python cygnss_camp2ex.py ' + main_input_filename)
@@ -73,9 +73,10 @@ os.chdir('out')
 filename_out = '/Users/cbv/cygnss/camp2ex/toshare/' + output_dir_no_path + '.tgz'
 os.system('tar -zcvf ' + filename_out + ' ' +  output_dir_no_path)
 
-os.system('/usr/bin/uuencode ' + filename_out + ' ' + filename_out + ' | /usr/bin/mail -s "me CYGNSS position by SpOCK - Camp2Ex - ' + date_start + ' to ' + date_stop + '" cbv@umich.edu')
+os.system('/usr/bin/uuencode ' + filename_out + ' ' + filename_out + ' | /usr/bin/mail -s "me5 CYGNSS positions by SpOCK - Camp2Ex - ' + date_start + ' to ' + date_stop + '" cbv@umich.edu')
 
-#os.system('/usr/bin/uuencode /Users/cbv/cygnss/camp2ex/' + filename_out.replace(".txt", ".tgz") + ' /Users/cbv/cygnss/camp2ex/' + filename_out.replace(".txt", ".tgz") + ' | /usr/bin/mail -s "CYGNSS SP positions by SpOCK - PISTON - ' + datetime.strptime(date_start, "%Y-%m-%dT%H:%M:%S").strftime('%d %B')[:6] + '" timothy.j.landdddg@nasa.gov')
+os.system('/usr/bin/uuencode ' + filename_out + ' ' + filename_out + ' | /usr/bin/mail -s "CYGNSS positions by SpOCK - Camp2Ex - ' + date_start + ' to ' + date_stop + '" roman.s.kowch@nasa.gov')
 
 #os.system('scp -p /Users/cbv/cygnss/camp2ex/' + filename_out.replace(".txt", ".tgz") + ' cygnss-sftp-1.engin.umich.edu:/data/temp/piston')
 
+os.chdir('../')
