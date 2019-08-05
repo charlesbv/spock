0a0d0abe5ffb2eb0960d37e83a4501809a4932d1
cbv <cbv@srbwks2014-0008.engin.umich.edu>
2019-08-05 08:14:08 -0400

    ign

--- srcPython/all_pid_algo_v2.py
+++ srcPython/all_pid_algo_v2.py
@@ -2,8 +2,8 @@
 # THis script plots the distance, amplitude, orbit average of runs amde with pid_algo_v2.py. The pickle were saved in pid_algo_v2.py
 # inputs: pickle_root_list stores each pickle to load (one per run in pid_algo_v2.py) (the pickles are assumed ot be in ./pickle)
 # (pickle_root =  prefix_name + '_' + rho_more in pid_algo_v2.py)
-pickle_root_list = ['FM8_20170901_omniweb_mid', 'FM8_20170901_again_mid', 'FM8_20170901_no_storm_mid'] #['FM8_20170901_mid']
-label_overwrite = ['Omniweb', 'SWPC', 'No storm']
+pickle_root_list = ['FM7_20170901_mid']#, 'FM8_20170901_omniweb_mid']# ['FM8_20170901_omniweb_mid', 'FM8_20170901_again_mid', 'FM8_20170901_no_storm_mid'] #['FM8_20170901_mid']
+label_overwrite = ['FM07', 'FM08']#['Omniweb', 'SWPC', 'No storm']
 #['FM4_20180112_fine_mid'] ['FM4_20180112_mid']
 #["fm01_20170817_mid"] 
 # ["fm4_mid", "test_mid", "2018jan12_mid", "nadir"]
@@ -17,7 +17,7 @@ label_overwrite = ['Omniweb', 'SWPC', 'No storm']
 #['localtime70percent_mid']#['localtime_pole', 'localtime_equator', 'localtime70percent_mid']
 #['solarzenith_equator', 'solarzenith_pole', 'localtime70percent_mid']# ['localtime70percentAp2_mid']#
 
-toplot = 'rho_control' # raw, amplitude, rho_control, rho
+toplot = 'rho' # raw, amplitude, rho_control, rho
 color_arr = ['blue', 'red', 'black' ,'mediumorchid', 'dodgerblue', 'magenta', 'darkgreen', 'limegreen'] #['blue', 'red', 'green', 'black', 'magenta']
 isbig = 0
 ispleiades = 0
